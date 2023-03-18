771f2008afcf26358d38a86c812329aacbd08a91
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2023-03-18 14:46:18 -0500

    Addressed deployment error

--- public/DetectThemePreference.tsx
+++ public/DetectThemePreference.tsx
@@ -1,4 +1,7 @@
 export default function prefersDark(): boolean {
+  if (window) {
     const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
     return darkThemeMq.matches ? true : false;
+  }
+  return true;
 }
