1e39b09df05fe5b83b596d52d6ff06fb3ffe3607
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2023-03-18 15:37:29 -0500

    Revert "Addressed deployment error"

--- public/DetectThemePreference.tsx
+++ public/DetectThemePreference.tsx
@@ -1,7 +1,4 @@
 export default function prefersDark(): boolean {
-  if (typeof window !== "undefined") {
     const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
     return darkThemeMq.matches ? true : false;
-  }
-  return true;
 }
