51404a07de5f6c04205be420b9d2626e7142d92f
taylorwaddell <waddell.taylorw@gmail.com>
2023-03-18 19:06:17 -0500

    This might work

--- public/Tabs.tsx
+++ public/Tabs.tsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
-import prefersDark from "./DetectThemePreference";
-import styled from "styled-components";
+// import prefersDark from "./DetectThemePreference";
+import styled, {css} from "styled-components";
 
 const tabsData = [
   { title: "github", link: "https://github.com/taylorwaddell", value: 1 },
@@ -18,14 +18,19 @@ const Tabs = () => {
   const [highlightedTab, setHighlightedTab] = useState(null);
   const [isHoveredFromNull, setIsHoveredFromNull] = useState(true);
   const [isBeingPressed, setIsBeingPressed] = useState(false);
-  const [prefersDark, setPrefersDark] = useState(true);
+  const [prefersDark, setPrefersDark] = useState(false);
 
   const highlightRef = useRef(null);
   const wrapperRef = useRef(null);
 
   useEffect(() => {
     const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    setPrefersDark(() => darkThemeMq.matches ? true : false);
+    if (darkThemeMq.matches) {
+      setPrefersDark(() => true);
+      return;
+    } else {
+      setPrefersDark(() => false);
+    }
   }, []);
 
   const repositionHighlight = (e: MouseEvent, tab: unknown) => {
@@ -47,22 +52,21 @@ const Tabs = () => {
       tabBoundingBox.left - wrapperBoundingBox.left
     }px)`;
     if (isBeingPressed) {
-      highlightStyles.boxShadow = `inset 1px 2px 4px 1px ${
-        prefersDark ? "rgb(29, 29, 29)" : "rgb(229, 229, 229)"
-      }`;
+      highlightStyles.boxShadow = `inset 1px 2px 4px 1px ${prefersDark ? 'rgb(29, 29, 29)' : 'rgb(229, 229, 229)'}`;
     }
   }
 
   return (
     <ContainerFlexColumn>
-      <TabsNav ref={wrapperRef} onMouseLeave={resetHighlight}>
-        <TabsHighlight ref={highlightRef} style={highlightStyles} />
+      <TabsNav prefersDark={prefersDark} ref={wrapperRef} onMouseLeave={resetHighlight}>
+        <TabsHighlight prefersDark={prefersDark} ref={highlightRef} style={highlightStyles} />
         {tabsData.map((tab) => (
           <Tab
             href={tab.link}
             target="_blank"
             key={tab.value}
             tabValue={tab.value}
+            prefersDark={prefersDark}
             onMouseOver={(ev) => repositionHighlight(ev, tab)}
             onMouseDown={() => setIsBeingPressed(() => true)}
             onMouseUp={() => {
@@ -85,26 +89,42 @@ interface TabProps {
 
 const TabsNav = styled.div`
   position: relative;
-  color: ${prefersDark ? "rgb(229, 229, 229)" : "rgb(29, 29, 29)"};
-  background-color: ${prefersDark ? "rgb(220, 220, 220)" : "rgb(20, 20, 20)"};
   border-radius: 10px;
   padding: 5px;
+
+  ${(props) => {
+    if (props.prefersDark) {
+      return css`
+        background-color: rgb(20, 20, 20);
+        color: rgb(229, 229, 229);
+      `;
+    } else {
+      return css`
+        background-color: rgb(220, 220, 220);
+        color: rgb(29, 29, 29);
+      `;
+    }
+  }}
 `;
 
 const Tab = styled.a<TabProps>`
   padding: 15px;
   font-size: ${14 / 16}rem;
-  color: ${prefersDark ? "rgb(229, 229, 229)" : "rgb(29, 29, 29)"};
   display: inline-block;
   position: relative;
   cursor: pointer;
   transition: color 250ms;
   height: 100%;
   user-select: none;
+
+  ${(props) => {
+      return css`
+        color: ${props.prefersDark ? 'rgb(229, 229, 229)' : 'rgb(29, 29, 29)'};
+      `;
+  }}
 `;
 
 const TabsHighlight = styled.div`
-  background: ${prefersDark ? "rgb(90, 90, 90)" : "rgb(290, 290, 290)"};
   position: absolute;
   top: 4.5px;
   left: 0;
@@ -112,7 +132,20 @@ const TabsHighlight = styled.div`
   height: 46px;
   transition: 0.15s ease;
   transition-property: width, transform, opacity;
-  border: 1px ${prefersDark ? "rgb(20, 20, 20)" : "rgb(220, 220, 220)"} solid;
+
+  ${(props) => {
+    if (props.prefersDark) {
+      return css`
+        background-color: rgb(90, 90, 90);
+        border: 1px rgb(20, 20, 20) solid;
+      `;
+    } else {
+      return css`
+        background-color: rgb(290, 290, 290);
+        border: 1px rgb(220, 220, 220) solid;
+      `;
+    }
+  }}
 `;
 
 const ContainerFlexColumn = styled.div`
